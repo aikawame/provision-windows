7a7f170dc3c7dbfbc363e1739a8194772f1b1257
Masahiro Aikawa <aikawame@wor.jp>
2020-02-25 22:48:21 +0900

    :sparkles: レジストリファイルを追加する

--- provision.bat
+++ provision.bat
@@ -2,6 +2,8 @@ powershell -ExecutionPolicy RemoteSigned .\scripts\set_appx_packages.ps1
 powershell -ExecutionPolicy RemoteSigned .\scripts\set_optional_features.ps1
 
 reg import .\registrations\settings.reg
+reg import .\registrations\control_panel.reg
+reg import .\registrations\sounds.reg
 reg import .\registrations\others.reg
 
 pause
