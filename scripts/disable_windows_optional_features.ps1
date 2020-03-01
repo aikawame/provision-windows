722cf84ce6910f4c8c462191f9f0f1cd1ff99b82
Masahiro Aikawa <aikawame@wor.jp>
2020-03-01 11:26:16 +0900

    :adhesive_bandage: 無効にするWindowsの機能を追加する

--- scripts/disable_windows_optional_features.ps1
+++ scripts/disable_windows_optional_features.ps1
@@ -1,11 +1,15 @@
 $packages = @(
 'Internet-Explorer-Optional-amd64',
 'MediaPlayback',
+'MicrosoftWindowsPowerShellV2Root',
+'NetFx4-AdvSrvs',
+'Printing-Foundation-Features',
 'Printing-XPSServices-Features',
 'SMB1Protocol',
 'SMB1Protocol-Client'
 'SMB1Protocol-Deprecation',
-'WindowsMediaPlayer'
+'WindowsMediaPlayer',
+'WorkFolders-Client'
 )
 
 $packages | % { Disable-WindowsOptionalFeature -NoRestart -Online -FeatureName $_ }
