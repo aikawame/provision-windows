7f6ddc97d68db23f56cb5ae82e7448778cb69ff9
Masahiro Aikawa <aikawame@wor.jp>
2020-03-23 22:31:42 +0900

    :art: Windowsの機能の有効化・無効化を無人セットアップに移動する

--- install.ps1
+++ install.ps1
@@ -1,13 +1,12 @@
 Set-ExecutionPolicy RemoteSigned -Force
+$ErrorActionPreference = 'Stop'
 
 Set-Location -Path C:\Windows\Temp
-Invoke-WebRequest -Uri https://github.com/aikawame/provision-windows/archive/master.zip -OutFile provision-windows-master.zip
+Invoke-WebRequest -Uri https://github.com/aikawame/provision-windows/archive/master.zip -OutFile provision-windows-master.zip -UseBasicParsing
 Expand-Archive -Path .\provision-windows-master.zip -DestinationPath .\ -Force
 Set-Location -Path .\provision-windows-master
 
 .\scripts\remove_appx_packages.ps1
-.\scripts\disable_windows_optional_features.ps1
-.\scripts\enable_windows_optional_features.ps1
 .\scripts\install_package_providers.ps1
 .\scripts\install_packages.ps1
 
