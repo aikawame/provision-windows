81e7204f1d4b948cf10b11ff06e12fd36eb86e33
Masahiro Aikawa <aikawame@wor.jp>
2020-03-30 00:00:27 +0900

    :sparkles: ATOK Passportのインストール処理を追加する

--- scripts/install_packages.ps1
+++ scripts/install_packages.ps1
@@ -31,6 +31,21 @@ function Install-Package-Retryable($package, $errorCount = 0) {
   }
 }
 
+function Install-AtokPassport() {
+  Write-Host '-> AtokPassport'
+  $client = New-Object net.webclient
+  $client.DownloadFile('https://sevenzip.osdn.jp/howto/9.20/7z.exe', '7z.exe')
+  $client.DownloadFile('https://sevenzip.osdn.jp/howto/9.20/7z.dll', '7z.dll')
+  $client.DownloadFile('https://gate.justsystems.com/download/atok/ut/win/at31try4.exe', 'at31try4.exe')
+  .\7z x at31try4.exe
+  .\at31try4\ATOK\SETUP.EXE /s
+  Remove-Item .\7z.exe
+  Remove-Item .\7z.dll
+  Remove-Item .\at31try4.exe
+  # TODO: ウェイトを入れてtry-catchを咬ませつつ消す
+  # Remove-Item .\at31try4 -Force -Recurse
+}
+
 function Install-TablePlus() {
   Write-Host '-> TablePlus'
   Invoke-WebRequest -Uri https://tableplus.com/release/windows/tableplus_latest -OutFile TablePlusSetup.exe -UseBasicParsing
@@ -43,6 +58,7 @@ $packages | % {
   Write-Host "-> $_"
   Install-Package-Retryable $_
 }
+Install-AtokPassport
 Install-TablePlus
 Write-Host ''
 
