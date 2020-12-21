2e7f318fa9622f95f5454831fff77ef395ac3338
Masahiro Aikawa <aikawame@wor.jp>
2020-12-21 22:27:44 +0900

    :art: 雑多な設定を分離する

--- install.ps1
+++ install.ps1
@@ -7,34 +7,31 @@ $client = New-Object net.webclient
 $client.DownloadFile('https://github.com/aikawame/provision-windows/archive/master.zip', "$env:temp\provision-windows-master.zip")
 Expand-Archive -Path .\provision-windows-master.zip -DestinationPath .\ -Force
 Set-Location -Path .\provision-windows-master
-
+Write-Host ''
 .\scripts\remove_appx_packages.ps1
 .\scripts\install_package_providers.ps1
 .\scripts\install_packages.ps1
 .\scripts\update_reg_acls.ps1
-
+Write-Host ''
 Write-Host 'Applying registry:'
 Reg import .\registrations\settings.reg
 Reg import .\registrations\control_panel.reg
 Reg import .\registrations\sounds.reg
 Reg import .\registrations\others.reg
 Write-Host ''
-Write-Host 'Applying other settings:'
-net accounts /maxpwage:unlimited
-Write-Host ''
 Write-Host 'Provisioning WSL environment:'
 wsl DEBIAN_FRONTEND=noninteractive apt-get update
 wsl DEBIAN_FRONTEND=noninteractive apt-get install -y language-pack-ja ansible
 wsl update-locale LANG=ja_JP.UTF8
 wsl DEBIAN_FRONTEND=noninteractive dpkg-reconfigure tzdata
-
+Write-Host ''
 wsl ansible-playbook -i local local.yml --ask-vault-pass
 Write-Host ''
-
 Write-Host 'Increase max_map_count for Elasticsearch container:'
 wsl -d docker-desktop echo 262144 ^> /proc/sys/vm/max_map_count
 Write-Host ''
-
+.\scripts\others.ps1
+Write-Host ''
 # Set-Location -Path $env:temp
 # Remove-Item .\provision-windows-master.zip
 # Remove-Item .\provision-windows-master -Recurse
---
+++ scripts/others.ps1
@@ -0,0 +1,4 @@
+Write-Host 'Applying other settings:'
+
+net accounts /maxpwage:unlimited
+
