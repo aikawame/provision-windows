3153d035b5be5e0e4ff80edc20b0f46b62d3f2b9
Masahiro Aikawa <aikawame@wor.jp>
2021-01-24 15:27:40 +0900

    :adhesive_bandage: フォントのインストールをリンクではなくコピーに変更する

--- scripts/others.ps1
+++ scripts/others.ps1
@@ -5,6 +5,6 @@ net accounts /maxpwage:unlimited
 New-Item -Type SymbolicLink C:\Users\aikawame\AppData\Roaming\Keyhac -Value "\\wsl$\Ubuntu-20.04\root\src\appdata-windows\Keyhac"
 New-Item -Type SymbolicLink C:\Users\aikawame\AppData\Roaming\Powerman -Value "\\wsl$\Ubuntu-20.04\root\src\appdata-windows\Powerman"
 New-Item -Type SymbolicLink C:\Users\aikawame\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Value "\\wsl$\Ubuntu-20.04\root\src\appdata-windows\Terminal\settings.json"
-New-Item -Type SymbolicLink C:\Windows\Fonts\Ricty-Bold.ttf -Value "\\wsl$\Ubuntu-20.04\root\src\appdata-windows\Fonts\Ricty-Bold.ttf"
+Copy-Item \\wsl$\Ubuntu-20.04\root\src\appdata-windows\Fonts\Ricty-Bold.ttf C:\Windows\Fonts\Ricty-Bold.ttf
 
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "Ricty Bold (TrueType)" /t REG_SZ /d Ricty-Bold.ttf /f
