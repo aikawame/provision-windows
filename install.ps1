3eb449dcf3025be5c7ec2e5ec7202e8b122314aa
Masahiro Aikawa <aikawame@wor.jp>
2021-09-25 15:48:41 +0900

    :sparkles: UACの無効化処理を追加する

--- install.ps1
+++ install.ps1
@@ -3,6 +3,8 @@ Write-Host 'PowerShellを設定しています...'
 Set-ExecutionPolicy RemoteSigned -Force
 Enable-PSRemoting -Force -SkipNetworkProfileCheck
 $ErrorActionPreference = 'Stop'
+$path = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System'
+Set-ItemProperty -Path $path -Name 'EnableLUA' -Value 0
 
 Write-Host ''
 Write-Host 'WSLをインストールしています...'
@@ -14,7 +16,7 @@ $path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce'
 $name = 'Provisioning'
 $script = 'https://raw.githubusercontent.com/aikawame/provision-windows/main/main.ps1'
 $value = "powershell -ExecutionPolicy Bypass ((New-Object Net.WebClient).DownloadString('$script') | iex)"
-Set-ItemProperty -path $path -name $name -value $value
+Set-ItemProperty -Path $path -Name $name -Value $value
 
 Write-Host ''
 Write-Host 'コンピューターを再起動しています...'
