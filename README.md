a98fb79b8c7e96ad4d48dc6d5030d4ae7cc68e6d
Masahiro Aikawa <aikawame@wor.jp>
2021-09-25 19:58:02 +0900

    :adhesive_bandage: インストールの流れを調整する

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # provision-windows
 
-## インストール
+## WSLのインストール
+
+以下のスクリプトを実行します。
+
+最後にUACの再起動ダイアログが出現するので、クリックして再起動します。
 
 ```powershell
 powershell -ExecutionPolicy Bypass ((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/aikawame/provision-windows/main/install.ps1') | iex)
 ```
 
-途中でPCが自動的に再起動し、その後の処理でAnsible Vaultのパスワードを求められます。
+## WSLの初期化とAnsibleのインストール
+
+再起動すると、Ubuntuの初期設定画面が表示されるのでユーザーを作成します。
+
+その後、以下のスクリプトを実行すると、WSLの初期化とAnsibleのインストール、構成管理の適用を行います。
+
+```powershell
+powershell -ExecutionPolicy Bypass ((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/aikawame/provision-windows/main/main.ps1') | iex)
+```
--- install.ps1
+++ install.ps1
@@ -5,21 +5,15 @@ Write-Host 'PowerShellを設定しています...'
 Set-ExecutionPolicy RemoteSigned -Force
 Enable-PSRemoting -Force -SkipNetworkProfileCheck
 $ErrorActionPreference = 'Stop'
-$path = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System'
-Set-ItemProperty -Path $path -Name 'EnableLUA' -Value 0
 
 Write-Host ''
 Write-Host 'WSLをインストールしています...'
 wsl --install
 
 Write-Host ''
-Write-Host 'メイン処理を準備しています...'
-$path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce'
-$name = 'Provisioning'
-$script = 'https://raw.githubusercontent.com/aikawame/provision-windows/main/main.ps1'
-$value = "powershell -ExecutionPolicy Bypass ((New-Object Net.WebClient).DownloadString('$script') | iex)"
-Set-ItemProperty -Path $path -Name $name -Value $value
+Write-Host 'UACの設定を変更しています...'
+$path = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System'
+Set-ItemProperty -Path $path -Name 'EnableLUA' -Value 0
 
 Write-Host ''
-Write-Host 'コンピューターを再起動しています...'
-Restart-Computer -Force
+Write-Host 'Windowsのダイアログに従い、再起動してください。'
